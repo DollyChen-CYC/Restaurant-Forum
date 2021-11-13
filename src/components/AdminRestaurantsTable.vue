b0c23122e2b0b77671ab85bf4f8ed7177bec1196
DollyChen_CYC <dolly726033@gmail.com>
2021-11-14 01:06:49 +0800

    feat: add Admin dashboard

---
+++ src/components/AdminRestaurantsTable.vue
@@ -0,0 +1,69 @@
+<template>
+  <table class="table">
+    <thead class="table-dark">
+      <tr>
+        <th scope="col">
+          #
+        </th>
+        <th scope="col">
+          Category
+        </th>
+        <th scope="col">
+          Name
+        </th>
+        <th
+          scope="col"
+          width="300"
+          style="text-align: center"
+        >
+          操作
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="restaurant in restaurants"
+        :key="restaurant.id"
+      >
+        <th scope="row">
+          {{ restaurant.id }}
+        </th>
+        <td>{{ restaurant.Category ? restaurant.Category.name : '未分類' }}</td>
+        <td>{{ restaurant.name }}</td>
+        <td class="d-flex justify-content-between">
+          <a
+            href="#"
+            class="btn btn-link"
+          >Show</a>
+
+          <a
+            href="#"
+            class="btn btn-link"
+          >Edit</a>
+
+          <button
+            type="button"
+            class="btn btn-link"
+          >
+            Delete
+          </button>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+<script>
+export default {
+  props: {
+    initialRestaurants : {
+      type: Array,
+      required: true
+    }
+  },
+  data () {
+    return {
+      restaurants: this.initialRestaurants
+    }
+  }
+}
+</script>
