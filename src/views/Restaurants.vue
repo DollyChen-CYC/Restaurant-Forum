116f32562b9de5dd8a9b2db51eda724abb904de2
DollyChen_CYC <dolly726033@gmail.com>
2021-11-23 13:07:04 +0800

    feat: replace dummyData with restaurants API.

--- src/views/Restaurants.vue
+++ src/views/Restaurants.vue
@@ -15,7 +15,7 @@
 
     <!-- 分頁標籤 RestaurantPagination -->
     <RestaurantsPagination
-      show-if="totalPage.length > 1"
+      v-if="totalPage.length > 1"
       :currentPage="currentPage"
       :totalPage="totalPage"
       :nextPage="nextPage"
@@ -26,284 +26,12 @@
 </template>
 
 <script>
-import NavTabs from "../components/NavTabs.vue";
-import RestaurantCards from "../components/RestaurantCards.vue";
-import RestaurantsNavPhills from "../components/RestaurantsNavPhills.vue";
-import RestaurantsPagination from "../components/RestaurantsPagination.vue";
-
-const dummyData = {
-  restaurants: [
-    {
-      id: 1,
-      name: "Ms. Victor Hane",
-      tel: "374.151.0927 x63501",
-      address: "3820 Robel Rapid",
-      opening_hours: "08:00",
-      description: "Aut aliquam distinctio inventore architecto eius e",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=41.13090251119787",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-    {
-      id: 2,
-      name: "Juliana McDermott",
-      tel: "1-924-686-9178 x931",
-      address: "359 Marks Throughway",
-      opening_hours: "08:00",
-      description: "culpa porro et",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=6.309807090823716",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 5,
-      Category: {
-        id: 5,
-        name: "素食料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-    {
-      id: 3,
-      name: "Weston Walsh",
-      tel: "067-797-0416 x066",
-      address: "88349 Von Islands",
-      opening_hours: "08:00",
-      description: "illo",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=29.183465918710482",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: true,
-    },
-    {
-      id: 4,
-      name: "Lucie Rowe",
-      tel: "677-014-4963",
-      address: "7700 Marquardt Islands",
-      opening_hours: "08:00",
-      description: "Quis vel nulla sed. Sed ad nobis quibusdam ea reic",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=83.14571419845818",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 2,
-      Category: {
-        id: 2,
-        name: "日本料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-    {
-      id: 5,
-      name: "Claire Green",
-      tel: "1-877-541-4235 x817",
-      address: "351 Bernhard Branch",
-      opening_hours: "08:00",
-      description: "Minus sit voluptatem ea dolorem a voluptatem cupid",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=70.37382571838411",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-    {
-      id: 6,
-      name: "Amie Herman",
-      tel: "1-077-082-8890",
-      address: "730 Schulist Corners",
-      opening_hours: "08:00",
-      description: "veniam",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=42.99561100271268",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: true,
-      isLiked: false,
-    },
-    {
-      id: 7,
-      name: "Efren Dietrich",
-      tel: "551-633-9230",
-      address: "721 Balistreri Station",
-      opening_hours: "08:00",
-      description: "Ipsum explicabo quas ducimus dolorum sit impedit a",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=82.83936040920796",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 3,
-      Category: {
-        id: 3,
-        name: "義大利料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-    {
-      id: 8,
-      name: "Kaden Herzog",
-      tel: "(694) 129-2150 x96275",
-      address: "370 Kub Green",
-      opening_hours: "08:00",
-      description: "Quis deserunt tempora repellendus.",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=87.6135623863038",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-    {
-      id: 9,
-      name: "Derrick Koss",
-      tel: "840-519-8657 x94847",
-      address: "6367 Noah Spur",
-      opening_hours: "08:00",
-      description: "quos",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=20.292652942039567",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 4,
-      Category: {
-        id: 4,
-        name: "墨西哥料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: true,
-      isLiked: true,
-    },
-    {
-      id: 10,
-      name: "Mrs. Christ Schaefer",
-      tel: "704.749.2163 x94510",
-      address: "6778 Will Overpass",
-      opening_hours: "08:00",
-      description: "molestiae aut aut",
-      image:
-        "https://loremflickr.com/320/240/restaurant,cuisine/?random=43.625304449012404",
-      viewCounts: 0,
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-      CategoryId: 1,
-      Category: {
-        id: 1,
-        name: "中式料理",
-        createdAt: "2021-11-03T15:01:36.000Z",
-        updatedAt: "2021-11-03T15:01:36.000Z",
-      },
-      isFavorited: false,
-      isLiked: false,
-    },
-  ],
-  categories: [
-    {
-      id: 1,
-      name: "中式料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-    {
-      id: 2,
-      name: "日本料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-    {
-      id: 3,
-      name: "義大利料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-    {
-      id: 4,
-      name: "墨西哥料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-    {
-      id: 5,
-      name: "素食料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-    {
-      id: 6,
-      name: "美式料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-    {
-      id: 7,
-      name: "複合式料理",
-      createdAt: "2021-11-03T15:01:36.000Z",
-      updatedAt: "2021-11-03T15:01:36.000Z",
-    },
-  ],
-  categoryId: "",
-  page: 1,
-  totalPage: [1, 2, 3, 4, 5],
-  prev: 1,
-  next: 2,
-};
+import NavTabs from '../components/NavTabs.vue'
+import RestaurantCards from '../components/RestaurantCards.vue'
+import RestaurantsNavPhills from '../components/RestaurantsNavPhills.vue'
+import RestaurantsPagination from '../components/RestaurantsPagination.vue'
+import restaurantsAPI from '../apis/restaurants.js'
+import { Toast } from '../utils/helpers.js'
 
 export default {
   components: {
@@ -312,7 +40,7 @@ export default {
     RestaurantsNavPhills,
     RestaurantsPagination,
   },
-  data() {
+  data () {
     return {
       restaurants: [],
       categories: [],
@@ -321,29 +49,60 @@ export default {
       totalPage: [],
       previousPage: -1,
       nextPage: -1,
-    };
+    }
   },
-  created() {
-    this.fetchRestaurants();
+  created () {
+    const { page = '', categoryId = '' } = this.$route.query
+    this.fetchRestaurants({
+      queryPage: page,
+      queryCategoryId: categoryId
+    })
+  },
+  beforeRouteUpdate (to, from, next) {
+    // console.log('to',to)
+    // console.log('from', from)
+    const { page = '', categoryId = '' } = to.query
+    this.fetchRestaurants({ queryPage: page, queryCategoryId: categoryId })
+    next()
   },
   methods: {
-    fetchRestaurants() {
-      const {
-        restaurants,
-        categories,
-        categoryId,
-        page,
-        totalPage,
-        prev,
-        next,
-      } = dummyData;
-      this.restaurants = restaurants;
-      this.categories = categories;
-      this.categoryId = categoryId;
-      this.page = page;
-      this.totalPage = totalPage;
-      this.prev = prev;
-      this.next = next;
+    async fetchRestaurants({ queryPage, queryCategoryId }) {
+      try {
+        const response = await restaurantsAPI.getRestaurants({
+          page: queryPage,
+          categoryId: queryCategoryId
+        })
+
+        // console.log('response', response)
+
+        if (response.statusText !== 'OK') {
+          throw new Error(response.statusText)
+        }
+
+        const {
+          restaurants,
+          categories,
+          categoryId,
+          page,
+          totalPage,
+          prev,
+          next,
+        } = response.data
+        this.restaurants = restaurants
+        this.categories = categories
+        this.categoryId = categoryId
+        this.page = page
+        this.totalPage = totalPage
+        this.prev = prev
+        this.next = next
+
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試！'
+        })
+        console.log('error', error)
+      }
     },
   },
 };
