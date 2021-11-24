c45dd0f98bcf1e96fd8904a4ba58876b56d66313
DollyChen_CYC <dolly726033@gmail.com>
2021-11-24 15:29:54 +0800

    feat: add function to fetch top ranking restaurant list from API and modify the functions of adding/removing favorite restaurant to fit the API.

--- src/apis/restaurants.js
+++ src/apis/restaurants.js
@@ -1,16 +1,21 @@
-import { apiHelper  } from '../utils/helpers';
+import { apiHelper } from '../utils/helpers'
 const getToken = () => localStorage.getItem('token')
 
 export default {
   getRestaurants({ page, categoryID }) {
     const searchParams = new URLSearchParams({ page, categoryID })
     return apiHelper.get(`/restaurants?${searchParams}`, {
-      headers: { Authorization: `Bearer ${getToken()}`}
+      headers: { Authorization: `Bearer ${getToken()}` }
     })
   },
-  getFeeds () {
+  getFeeds() {
     return apiHelper.get('/restaurants/feeds', {
-      headers: { Authorization: `Bearer ${getToken()} `}
+      headers: { Authorization: `Bearer ${getToken()} ` }
+    })
+  },
+  getRestaurantsTop() {
+    return apiHelper.get(`/restaurants/top`, {
+      headers: { Authorization: `Bearer ${getToken()}` }
     })
   }
 }
