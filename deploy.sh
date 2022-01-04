3565cd57fc08d043236a58af8af4d29d8e6079fa
DollyChen_CYC <dolly726033@gmail.com>
2022-01-05 00:59:58 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.Github.io/<REPO>
 # git push -f https://github.com/<USERNAME>/<REPO>.git main:gh-pages
-git push -f https://github.com/DollyChen-CYC/Restaurant-Forum.git main:gh-pages
+git push -f https://github.com/DollyChen-CYC/Restaurant-Forum.git master:gh-pages
 
 cd -
